110d5726804dd22cc69948f32fd8df7ce5fca483
Vineeth B V <37930821+vinsdragonis@users.noreply.github.com>
2020-11-05 15:33:08 +0530

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 
 import scapy.all as scapy
+import argparse
+
+
+def get_range():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-t", "--target", dest="range", help="use this to set the ip range")
+    options = parser.parse_args()
+
+    if not options.range:
+        parser.error("[-] Please specify IP range, use --help for more info")
+
+    return options
 
 
 def scan(ip):
@@ -10,10 +23,21 @@ def scan(ip):
     answered_List = scapy.srp(arp_request_broadcast, timeout=1)[0]
 
     # print(answered_List.summary())
+    clients_list = []
+    for element in answered_List:
+        clients_Dict = {"ip": element[1].psrc, "mac": element[1].hwsrc}
+        clients_list.append(clients_Dict)
+    return clients_list
+
+
+def print_clients(clients):
     print("__________________________________")
     print(" IP\t\tMAC address\n----------------------------------")
-    for element in answered_List:
-        print(" " + element[1].psrc +"\t" + element[1].hwsrc)
+    for client in clients:
+        print(client["ip"] + "\t" + client["mac"])
+    print("----------------------------------")
 
 
-scan("10.0.2.1/24")
+ipRange = get_range()
+clients_List = scan(ipRange.range)
+print_clients(clients_List)
